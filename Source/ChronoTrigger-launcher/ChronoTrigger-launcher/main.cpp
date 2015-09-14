83d5581d7931111f16367a5eb293e6e2bd492212
kptnkrnch <jckptnkrnch@gmail.com>
2015-09-14 11:29:10 -0700

    Removed superfluous projects

--- Source/ChronoTrigger-launcher/ChronoTrigger-launcher/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	system("zsnesw.exe \"Chrono Trigger.smc\"");
-	return 0;
-}
--- Source/FinalFantasyVI-launcher/FinalFantasyVI-launcher/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	system("zsnesw.exe \"Final Fantasy III.smc\"");
-	return 0;
-}
